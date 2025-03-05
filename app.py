9f33fb21851d5cab33797e80a62be6ce33b8c9de
Tzu-Jo Hsu <tzujohsu@umich.edu>
2025-03-05 11:24:38 +0800

    update layout and yml

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-# import sys
-# __import__('pysqlite3')
-# import pysqlite3
-# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+import sys
+__import__('pysqlite3')
+import pysqlite3
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import torch
 torch.classes.__path__ = []
--- data_pipeline.py
+++ data_pipeline.py
@@ -13,17 +13,17 @@ print('step 1: load the vector database done')
 print('\t# docs in the current database: ', len(docloader.vector_store.get()['documents']))
 
 
-# # step 2: 
+# step 2: 
 documents = docloader.load_documents_from_scraping(start_date=today_date , latest=True)
-# documents = docloader.load_documents_from_scraping(start_date=(datetime.now() - timedelta(days=4)), end_date= (datetime.now() - timedelta(days=35)), latest=False)
+# documents = docloader.load_documents_from_scraping(start_date=(datetime.now() - timedelta(days=2)), end_date= (datetime.now() - timedelta(days=35)), latest=False)
 print('step 2: scrape the latest news done')
 
-# # step 3:
+# step 3:
 docloader.load_documents_into_database(documents)
 print('step 3: load the documents into db done')
 print('\t# docs in the current database: ', len(docloader.vector_store.get()['documents']))
 
-# # step 4
-# docloader.remove_documents(cutoff_threshold=60)
-# print('step 4: remove the oldest transcripts done')
-# print('\t# docs in the current database: ', len(docloader.vector_store.get()['documents']))
+# step 4
+docloader.remove_documents(cutoff_threshold=35)
+print('step 4: remove the oldest transcripts done')
+print('\t# docs in the current database: ', len(docloader.vector_store.get()['documents']))
